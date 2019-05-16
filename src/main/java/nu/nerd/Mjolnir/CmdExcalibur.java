5bdfdae3c19780349294ea24ec5d1fb7762563db
totemo <TotemoResearch@gmail.com>
2019-05-16 19:36:32 +0930

    Fix issue #1: NPE in /excalibur.

--- src/main/java/nu/nerd/Mjolnir/CmdExcalibur.java
+++ src/main/java/nu/nerd/Mjolnir/CmdExcalibur.java
@@ -17,8 +17,8 @@ import org.bukkit.entity.Player;
 public class CmdExcalibur implements CommandExecutor {
 
     @SuppressWarnings("unused")
-    private MjolnirPlugin plugin;
-    
+    private final MjolnirPlugin plugin;
+
     public CmdExcalibur(MjolnirPlugin plugin) {
         this.plugin = plugin;
     }
@@ -32,41 +32,42 @@ public class CmdExcalibur implements CommandExecutor {
             sender.sendMessage(ChatColor.RED + "Only players can use the power of Excalibur.");
             return true;
         }
-        Player player = (Player)sender;
-        
+        Player player = (Player) sender;
+
         // Parse arguments
         String msg = Util.Msgify(0, args);
 
-        // Get the block the player is looking at, using the same search distance as Command Helper.
+        // Get the block the player is looking at, using the same search
+        // distance as Command Helper.
         // We cast the null to call the correct overload of getTargetBlock.
-        Block block = player.getTargetBlock((Set<Material>)null, 10000);
-        if(block == null) {
+        Block block = player.getTargetBlock((Set<Material>) null, 10000);
+        if (block == null) {
             player.sendMessage(ChatColor.RED + "No block in sight, or block too far.");
             return true;
         }
-        
+
         World w = player.getWorld();
         int x = block.getX();
         int y = block.getY();
         int z = block.getZ();
-        
+
         w.strikeLightning(new Location(w, x + 2, y, z));
         w.strikeLightning(new Location(w, x - 2, y, z));
         w.strikeLightning(new Location(w, x, y, z + 2));
         w.strikeLightning(new Location(w, x, y, z - 2));
         w.createExplosion(x, y, z, 8, true, true);
-        
+
         // /explode everything in the vicinity
         for (Entity entity : w.getNearbyEntities(new Location(w, x, y, z), 3, 3, 3)) {
-            if(entity instanceof LivingEntity) {
-                Util.Explode((LivingEntity)entity, false);
-                if(entity instanceof Player) {
-                    Player p = (Player)entity;
+            if (entity instanceof LivingEntity) {
+                Util.Explode((LivingEntity) entity, false);
+                if (msg != null && entity instanceof Player) {
+                    Player p = (Player) entity;
                     p.sendMessage(ChatColor.RED + msg);
                 }
             }
         }
-        
+
         return true;
     }
 

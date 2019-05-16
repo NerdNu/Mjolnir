169b016acb680deca916f31a2df19e7700f1b3f3
totemo <TotemoResearch@gmail.com>
2019-05-16 19:38:42 +0930

    Make `/striket` send non-lethal lightning effects...
    ...for consistency with the other `/strike*` variants.

--- src/main/java/nu/nerd/Mjolnir/CmdStriket.java
+++ src/main/java/nu/nerd/Mjolnir/CmdStriket.java
@@ -13,10 +13,10 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class CmdStriket implements CommandExecutor {
-    
+
     @SuppressWarnings("unused")
-    private MjolnirPlugin plugin;
-    
+    private final MjolnirPlugin plugin;
+
     public CmdStriket(MjolnirPlugin plugin) {
         this.plugin = plugin;
     }
@@ -31,29 +31,30 @@ public class CmdStriket implements CommandExecutor {
             sender.sendMessage(ChatColor.RED + "Only players can haz the powerz of striket.");
             return true;
         }
-        Player player = (Player)sender;
-        
+        Player player = (Player) sender;
+
         // Check args
         if (args.length != 0) {
             sender.sendMessage(ChatColor.RED + "USAGE: /striket");
             return true;
         }
-        
-        // Get the block the player is looking at, using the same search distance as Command Helper.
+
+        // Get the block the player is looking at, using the same search
+        // distance as Command Helper.
         // We cast the null to call the correct overload of getTargetBlock.
-        Block block = player.getTargetBlock((Set<Material>)null, 10000);
-        if(block == null) {
+        Block block = player.getTargetBlock((Set<Material>) null, 10000);
+        if (block == null) {
             player.sendMessage(ChatColor.RED + "No block in sight, or block too far.");
             return true;
         }
-        
+
         // Call down the thunder
         World w = block.getWorld();
         int x = block.getX();
         int y = block.getY() + 1;
         int z = block.getZ();
         Location loc = new Location(w, x, y, z);
-        w.strikeLightning(loc);
+        w.strikeLightningEffect(loc);
 
         return true;
     }

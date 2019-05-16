8d224d775e1358a39abb9b84d34f5dd41f22afbf
totemo <TotemoResearch@gmail.com>
2019-05-16 19:37:39 +0930

    Fix double-sent message in /armageddon.

--- src/main/java/nu/nerd/Mjolnir/CmdArmageddon.java
+++ src/main/java/nu/nerd/Mjolnir/CmdArmageddon.java
@@ -1,6 +1,5 @@
 package nu.nerd.Mjolnir;
 
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,8 +7,8 @@ import org.bukkit.entity.Player;
 
 public class CmdArmageddon implements CommandExecutor {
 
-    private MjolnirPlugin plugin;
-    
+    private final MjolnirPlugin plugin;
+
     public CmdArmageddon(MjolnirPlugin plugin) {
         this.plugin = plugin;
     }
@@ -21,13 +20,10 @@ public class CmdArmageddon implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         // Parse arguments
         String msg = Util.Msgify(0, args);
-        
+
         // Now call lightning on everyone!
-        for(Player selectedPlayer : plugin.getServer().getOnlinePlayers()) {
+        for (Player selectedPlayer : plugin.getServer().getOnlinePlayers()) {
             Util.Thor(selectedPlayer, msg);
-        }
-        if(msg != null) {
-            plugin.getServer().broadcastMessage(ChatColor.RED + msg);
         }
         return true;
     }
